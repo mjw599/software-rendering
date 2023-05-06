16624c04468af62157cbf9c44c6183111c2b0410
Mark Wood <mjw599@gmail.com>
2023-05-06 18:38:20 +0100

    Switch from SDL to use winit and softbuffer for a pure rust implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::rect::Rect;
-
 use crate::model::Model;
 use crate::vector::Vec3f;
 use crate::triangle_renderer::VertexData;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder, dpi::PhysicalSize,
+};
+use softbuffer::GraphicsContext;
 
 const WIDTH: u32 = 320;
 const HEIGHT: u32 = 240;
@@ -15,39 +17,17 @@ mod model;
 mod triangle_renderer;
 
 fn main() {
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().with_inner_size(PhysicalSize::new(WIDTH, HEIGHT)).build(&event_loop).unwrap();
+
+    let mut context = unsafe { GraphicsContext::new(&window, &window) }.unwrap();
+
     let model = Model::parse(&String::from("data/models/african_head.obj"), &String::from("data/models/african_head_diffuse.tga")).unwrap();
-
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem
-        .window("Game", WIDTH, HEIGHT)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window
-        .into_canvas()
-        //.present_vsync()
-        .build()
-        .unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-
-    let tex_creator = canvas.texture_creator();
-    let mut game_tex = tex_creator
-        .create_texture(
-            sdl2::pixels::PixelFormatEnum::ARGB8888,
-            sdl2::render::TextureAccess::Streaming,
-            WIDTH,
-            HEIGHT,
-        )
-        .expect("Couldn't create buffer");
 
     let mut framebuffer = vec![0u32; (WIDTH * HEIGHT) as usize].into_boxed_slice();
     let mut zbuffer = vec![0.0; (WIDTH * HEIGHT) as usize].into_boxed_slice();
     let mut horizontal_x = 0;
-    let update_rectangle = Rect::new(0, 0, WIDTH, HEIGHT);
-    let horizontal_pitch = 4 * WIDTH as usize;
     let half_height = (HEIGHT/2) as f64;
     let half_width = (WIDTH/2) as f64;
 
@@ -55,76 +35,64 @@ fn main() {
 
     let color_red = 0xFFFF0000 as u32;
 
-    'running: loop {
-        // Handle user input
-        // Escape key exits, everything else is ignored
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => {
-                    break 'running;
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_poll();
+
+        match event {
+            Event::MainEventsCleared => {
+                // Clear framebuffer
+                framebuffer.fill(0);
+                // Clear zbuffer
+                zbuffer.fill(f64::MIN);
+
+                for i in 0..model.faces.len() { 
+                    let face = &model.faces[i];
+                    let v1 = &model.vertices[face[0].vertex_index as usize];
+                    let v2 = &model.vertices[face[1].vertex_index as usize];
+                    let v3 = &model.vertices[face[2].vertex_index as usize];
+                    let screen1 = Vec3f{ x:((v1.x + 1.0) * half_width) + 0.5, y:((v1.y+1.0) * half_height) + 0.5, z:v1.z};
+                    let screen2 = Vec3f{ x:((v2.x + 1.0) * half_width) + 0.5, y:((v2.y+1.0) * half_height) + 0.5, z:v2.z};
+                    let screen3 = Vec3f{ x:((v3.x + 1.0) * half_width) + 0.5, y:((v3.y+1.0) * half_height) + 0.5, z:v3.z};
+
+                    let v1uv = &model.texture_coords[face[0].texture_index as usize];
+                    let v2uv = &model.texture_coords[face[1].texture_index as usize];
+                    let v3uv = &model.texture_coords[face[2].texture_index as usize];
+
+                    let vertex1 = VertexData { vertex: &screen1, texture: v1uv };
+                    let vertex2 = VertexData { vertex: &screen2, texture: v2uv };
+                    let vertex3 = VertexData { vertex: &screen3, texture: v3uv };
+
+                    let mut n = (v3-v1).cross(&(v2-v1));
+                    n = n.normalize();
+                    let intensity = n.dot(&light_dir);
+
+                    if intensity > 0.0 {
+                        triangle_renderer::render_triangle(&vertex1, &vertex2, &vertex3, &mut framebuffer, WIDTH as i64, HEIGHT as i64, &mut zbuffer, intensity, model.textures.get("diffuse").unwrap());
+                    }
                 }
-                _ => {}
+
+                draw_line(
+                    horizontal_x,
+                    13,
+                    40,
+                    80,
+                    &mut framebuffer,
+                    color_red,
+                );
+                horizontal_x = (horizontal_x + 1) % WIDTH as i32;
+
+                context.set_buffer(&framebuffer, WIDTH as u16, HEIGHT as u16);
             }
-        }
-
-        // Clear framebuffer
-        framebuffer.fill(0);
-        // Clear zbuffer
-        zbuffer.fill(f64::MIN);
-
-        for i in 0..model.faces.len() { 
-            let face = &model.faces[i];
-            let v1 = &model.vertices[face[0].vertex_index as usize];
-            let v2 = &model.vertices[face[1].vertex_index as usize];
-            let v3 = &model.vertices[face[2].vertex_index as usize];
-            let screen1 = Vec3f{ x:((v1.x + 1.0) * half_width) + 0.5, y:((v1.y+1.0) * half_height) + 0.5, z:v1.z};
-            let screen2 = Vec3f{ x:((v2.x + 1.0) * half_width) + 0.5, y:((v2.y+1.0) * half_height) + 0.5, z:v2.z};
-            let screen3 = Vec3f{ x:((v3.x + 1.0) * half_width) + 0.5, y:((v3.y+1.0) * half_height) + 0.5, z:v3.z};
-
-            let v1uv = &model.texture_coords[face[0].texture_index as usize];
-            let v2uv = &model.texture_coords[face[1].texture_index as usize];
-            let v3uv = &model.texture_coords[face[2].texture_index as usize];
-
-            let vertex1 = VertexData { vertex: &screen1, texture: v1uv };
-            let vertex2 = VertexData { vertex: &screen2, texture: v2uv };
-            let vertex3 = VertexData { vertex: &screen3, texture: v3uv };
-
-            let mut n = (v3-v1).cross(&(v2-v1));
-            n = n.normalize();
-            let intensity = n.dot(&light_dir);
-
-            if intensity > 0.0 {
-                triangle_renderer::render_triangle(&vertex1, &vertex2, &vertex3, &mut framebuffer, WIDTH as i64, HEIGHT as i64, &mut zbuffer, intensity, model.textures.get("diffuse").unwrap());
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => {
+                *control_flow = ControlFlow::Exit;
             }
+            _ => {}
         }
-
-        draw_line(
-            horizontal_x,
-            13,
-            40,
-            80,
-            &mut framebuffer,
-            color_red,
-        );
-        horizontal_x = (horizontal_x + 1) % WIDTH as i32;
-
-        // Update screen texture
-        game_tex
-            .update(
-                update_rectangle,
-                unsafe { &framebuffer.align_to().1 },
-                horizontal_pitch,
-            )
-            .unwrap();
-        // Copy texture to screen
-        canvas.copy(&game_tex, None, None).unwrap();
-        // Flip the screen buffers
-        canvas.present();
-    }
+    });
 }
 
 fn draw_line(
